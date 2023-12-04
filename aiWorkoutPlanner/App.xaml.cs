c3f096a51b5c80dd8b09138dd398b9d8c1faf65a
Joshua Hughes <Hughes.J@millfieldschool.com>
2023-12-04 09:45:03 +0000

    Add project files.

---
+++ aiWorkoutPlanner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace aiWorkoutPlanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ aiWorkoutPlanner/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace aiWorkoutPlanner
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
